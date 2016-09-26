fd120923b510200fef02562b2e4d3baa55200245
James Freeman <freeman.james.h@gmail.com>
2016-09-26 14:16:31 -0600

    styling

--- app.js
+++ app.js
@@ -19,15 +19,29 @@ app.controller('MainCtrl', ['$scope', function($scope) {
     upvotes: 4
   }];
 
-  $scope.addPost = function(){
-    if (!$scope.title || $scope.title === '') {return;}
-    $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link})
+  $scope.addPost = function() {
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      upvotes: 0,
+      link: $scope.link
+    })
     $scope.title = ''
     $scope.link = ''
   }
 
-  $scope.incrementUpvotes = function(post){
+  $scope.incrementUpvotes = function(post) {
     post.upvotes += 1
   }
+
+  app.factory('posts', [function() {
+
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
 
 }])
